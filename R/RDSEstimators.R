c0c353360764606a7858223d456f5ece68544a44
khabbazian <khabbazian@wisc.edu>
2016-01-12 12:29:44 -0500

    add rm na to HT and HH estimator

--- R/RDSEstimators.R
+++ R/RDSEstimators.R
@@ -1,45 +1,51 @@
 
 
-#' Sample mean estimator
-#'
 #'@param chain a sequence of node indices that represent the referral process.
 #'@param trait.vec is a vector of size number of nodes in the network with corresponding trait values.
-#'
-#'@export
-#'
 sample_mean  <- function(chain, trait.vec){
     return( mean(trait.vec[chain[,2]], na.rm=TRUE) )
 }
 
 
 
-#' HT estimator
-#'
 #'@param chain a sequence of node indices that represent the referral process.
 #'@param trait.vec is a vector of size number of nodes in the network with corresponding trait values.
 #'@param inclusion.probability is a vector of size number of nodes in the network with corresponding stationary probabilities.
-#'
-#'@export
-#'
 HT_mean  <- function(chain, trait.vec, inclusion.probability){
     stopifnot(length(inclusion.probability)==length(trait.vec))
-    i.p <- inclusion.probability[chain[,2]] 
-    return( sum(trait.vec[chain[,2]]/i.p, na.rm=TRUE)/sum(1/i.p) )
+    #NOTE: just the node sequence
+    chain <- chain[,2]
+    #NOTE: removing vertices with na trait.
+    chain <- chain[ -which(chain %in% which(is.na(trait.vec)) ) ]
+
+    if(length(chain) == 0)
+        return(NA)
+
+    stopifnot( length(which(is.na(trait.vec[chain]))) == 0)
+
+    i.p <- inclusion.probability[chain] 
+    return( sum(trait.vec[chain]/i.p, na.rm=TRUE)/sum(1/i.p) )
 }
 
 
 
-#' HH estimator
-#'
 #'@param chain a sequence of node indices that represent the referral process.
 #'@param trait.vec is a vector of size number of nodes in the network with corresponding trait values.
 #'@param inclusion.probability is a vector of size number of nodes in the network with corresponding stationary probabilities.
-#'
-#'@export
-#'
 HH_mean  <- function(chain, trait.vec, inclusion.probability){
     stopifnot(length(inclusion.probability)==length(trait.vec))
+    #NOTE: just the node sequence
     chain <- chain[,2]
+    #NOTE: removing vertices with na trait.
+    chain <- chain[ -which(chain %in% which(is.na(trait.vec)) ) ]
+
+    if(length(chain) == 0)
+        return(NA)
+
+    stopifnot( length(which(is.na(trait.vec[chain]))) == 0)
+
     i.p <- inclusion.probability[chain] 
     return( mean(trait.vec[chain]/i.p, na.rm=TRUE)/length(trait.vec) )
 }
+
+
