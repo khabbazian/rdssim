68233b5c33f74d7ce3479de589fb588ac6d1a854
khabbazian <khabbazian@wisc.edu>
2016-01-12 19:01:29 -0500

    change the doc

---
+++ R/interface.R
@@ -0,0 +1,24 @@
+#'
+#'Converts an adjacency matrix into the adjacency list
+#'
+#'@param A a symmetric unweighted adjacency matrix.
+#'@export 
+adj2list <- function(A) {
+    stopifnot( isSymmetric(A) )
+    .Call('rdssim_adj2list', PACKAGE = 'rdssim', A)
+}
+
+#'
+#'Simulates referral chains/trees for Respondent-driven sampling
+#'
+#'@param adjlist adjacency list of the input social network. It assumes that adjlist represents an undirected network. Refer to \code{adj2list}.
+#'@param rType referral type.
+#'@param nSamples number of samples to be collected.
+#'@param nReferrals number of referral per vertex.
+#'@param seedNode the starting vertex.
+#'@param rseed random seed for random number generators.
+#'@export 
+rdssim <- function(adjlist, rType, nSamples, nReferrals = 1L, seedNode = 1L, rseed = 1L) {
+    .Call('rdssim_rdssim_cpp', PACKAGE = 'rdssim', rcpp_adjlist, rType, nSamples, nReferrals, seedNode, rseed)
+}
+
