82ef131892a06619733c04c4dde121802817e9e2
khabbazian <khabbazian@wisc.edu>
2016-01-25 13:08:21 -0500

    change referral tree, add Markovian argument, clean the code

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -18,18 +18,19 @@ BEGIN_RCPP
 END_RCPP
 }
 // rdssim_cpp
-Rcpp::NumericMatrix rdssim_cpp(Rcpp::List rcpp_adjlist, std::string rType, int nSamples, int nReferrals, int seedNode, int rseed);
-RcppExport SEXP rdssim_rdssim_cpp(SEXP rcpp_adjlistSEXP, SEXP rTypeSEXP, SEXP nSamplesSEXP, SEXP nReferralsSEXP, SEXP seedNodeSEXP, SEXP rseedSEXP) {
+Rcpp::NumericMatrix rdssim_cpp(Rcpp::List rcpp_adjlist, std::string rType, bool Markovian, int nSamples, int nReferrals, int seedNode, int rseed);
+RcppExport SEXP rdssim_rdssim_cpp(SEXP rcpp_adjlistSEXP, SEXP rTypeSEXP, SEXP MarkovianSEXP, SEXP nSamplesSEXP, SEXP nReferralsSEXP, SEXP seedNodeSEXP, SEXP rseedSEXP) {
 BEGIN_RCPP
     Rcpp::RObject __result;
     Rcpp::RNGScope __rngScope;
     Rcpp::traits::input_parameter< Rcpp::List >::type rcpp_adjlist(rcpp_adjlistSEXP);
     Rcpp::traits::input_parameter< std::string >::type rType(rTypeSEXP);
+    Rcpp::traits::input_parameter< bool >::type Markovian(MarkovianSEXP);
     Rcpp::traits::input_parameter< int >::type nSamples(nSamplesSEXP);
     Rcpp::traits::input_parameter< int >::type nReferrals(nReferralsSEXP);
     Rcpp::traits::input_parameter< int >::type seedNode(seedNodeSEXP);
     Rcpp::traits::input_parameter< int >::type rseed(rseedSEXP);
-    __result = Rcpp::wrap(rdssim_cpp(rcpp_adjlist, rType, nSamples, nReferrals, seedNode, rseed));
+    __result = Rcpp::wrap(rdssim_cpp(rcpp_adjlist, rType, Markovian, nSamples, nReferrals, seedNode, rseed));
     return __result;
 END_RCPP
 }
